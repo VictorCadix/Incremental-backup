a466710866837e808e71c174ccbd5c4baefc3457
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2019-09-05 16:23:27 +0200

    backup struct and verbose flag

--- backupFunctions.py
+++ backupFunctions.py
@@ -3,48 +3,73 @@ import filecmp
 import shutil
 import datetime
 
-def compareDir (dir_new, dir_old, changes_list):
+class backup_struct:
+    new = []
+    deleted = []
+    
+    def __repr__(self):
+        
+        print("new :")
+        for item in self.new:
+            print('\t+' + item)
+        
+        print("deleted: ")
+        for item in self.deleted:
+            print('\t-' + item)
+        
+        return ''
+
+def compareDir (dir_new, dir_old, bckp_struct, verbose=False):
     cmp = filecmp.dircmp(dir_new, dir_old)
-    l_list = cmp.left_list
-    r_list = cmp.right_list
 
-    print('New:')
-    for item in l_list:
-        print('\t' + item)
+    #l_list = cmp.left_list
+    #r_list = cmp.right_list
 
-    print('Old:')
-    for item in r_list:
-        print('\t' + item)
+    #print('New:')
+    #for item in l_list:
+    #    print('\t' + item)
+
+    #print('Old:')
+    #for item in r_list:
+    #    print('\t' + item)
     
     new = cmp.left_only
     deleted = cmp.right_only
 
-    print('New:')
+    if verbose:
+        print('New:')
     for item in new:
-        changes_list['new'].append(dir_new + '/' + item)
-        print('\t' + item)
+        bckp_struct.new.append(dir_new + '/' + item)
+        if verbose:
+            print('\t' + item)
 
-    print('Deleted:')
+    if verbose:
+        print('Deleted:')
     for item in deleted:
-        changes_list['deleted'].append(dir_old + '/' + item)
-        print('\t' + item)
+        bckp_struct.deleted.append(dir_old + '/' + item)
+        if verbose:
+            print('\t' + item)
     
     # Compara los archivos con mismo nombre, si son diferentes guarda
     # el que esté en dir_new
-    print('common_files:')
+    if verbose:
+        print('common_files:')
     for item in cmp.common_files:
-        print('\t' + item)
+        if verbose:
+            print('\t' + item)
         areEqual = filecmp.cmp(dir_new + '/' + item, dir_old + '/' + item)
         #print('\t\t' + str(areEqual))
         
         if not areEqual:
-            changes_list['new'].append(dir_new + '/' + item)
+            bckp_struct.new.append(dir_new + '/' + item)
 
     # Para directorios comunes utiliza recursión
-    print('common_dirs:')
+    if verbose:
+        print('common_dirs:')
     for folder in cmp.common_dirs:
-        print('\t' + folder)
-        compareDir(dir_new + '/' + folder, dir_old + '/' + folder, changes_list)
+        if verbose:
+            print('\t' + folder)
+        compareDir(dir_new + '/' + folder, dir_old + '/' + folder, bckp_struct)
 
 
 def generate_incremental_backup(directory, changes_list):
@@ -89,4 +114,10 @@ def build_backup(backup_dir, gen_built_dir):
     print('Backup dates:')
     for i in backups:
         print('\t' + i)
-
+    
+    newFolder = gen_built_dir + '/' + 'Built'
+    #if not os.path.exists(newFolder):
+    #    os.makedirs(newFolder)
+    
+    for i in backups:
+        shutil.copy(backup_dir+'/'+i , newFolder)
