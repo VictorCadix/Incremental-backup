fc387052b42dc6b27223316a56a46d817ad9041c
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2019-09-08 14:38:14 +0200

    deleting commented prints

--- backupFunctions.py
+++ backupFunctions.py
@@ -25,17 +25,6 @@ class backup_struct:
 
 def compareDir (dir_new, dir_old, bckp_struct, verbose=False):
     cmp = filecmp.dircmp(dir_new, dir_old)
-
-    #l_list = cmp.left_list
-    #r_list = cmp.right_list
-
-    #print('New:')
-    #for item in l_list:
-    #    print('\t' + item)
-
-    #print('Old:')
-    #for item in r_list:
-    #    print('\t' + item)
     
     new = cmp.left_only
     deleted = cmp.right_only
